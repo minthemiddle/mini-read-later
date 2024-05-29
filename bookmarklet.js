1ec1e0ba44052e93fd210dad56dab00abb5cf6b8
Martin Betz <martinmichaelbetz@gmail.com>
2024-05-29 12:55:29 +0200

    Fix: Bookmarklet save URL construction

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,6 @@
 javascript: (function () {
   var url = window.location.href;
-  var readLaterUrl = "https://endpoint.com/" + encodeURIComponent(url);
+  var readLaterUrl = "SERVICEURL.com/save.php?url=" + url;
   fetch(readLaterUrl).then((response) => {
     if (response.ok) {
       alert("URL saved to Read Later");
